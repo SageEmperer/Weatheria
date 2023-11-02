85ffa885ace35f8bca9c1208c5d1d72fd2cbe608
SageEmperer <142967700+SageEmperer@users.noreply.github.com>
2023-11-02 21:26:55 +0530

    added an feature
    when search the city typed inside the search bar will be removed
    automatically

--- JS/script.js
+++ JS/script.js
@@ -16,23 +16,23 @@ let weather = {
     const { sunrise } = data.sys
     const { timezone } = data;
 
-    const sunriseUnixTimestamp = sunrise; // Replace with your Unix timestamp
-    const timezoneOffset = timezone; // Replace with your time zone's offset in seconds
+    const sunriseUnixTimestamp = sunrise; 
+    const timezoneOffset = timezone; 
 
-    const date = new Date(sunriseUnixTimestamp * 1000); // Convert to milliseconds
-    const utcTime = date.getTime() + date.getTimezoneOffset() * 60000; // Adjust for local time zone
-    const localTime = new Date(utcTime + timezoneOffset * 1000); // Adjust for the provided time zone offset
+    const date = new Date(sunriseUnixTimestamp * 1000); 
+    const utcTime = date.getTime() + date.getTimezoneOffset() * 60000;
+    const localTime = new Date(utcTime + timezoneOffset * 1000);
     const sunriseTime = localTime.toTimeString().slice(0, 8);
     // for sunset time
 
     const { sunset } = data.sys
 
-    const sunriseUnixTimestamps = sunset; // Replace with your Unix timestamp
-    const timezoneOffsets = timezone; // Replace with your time zone's offset in seconds
+    const sunriseUnixTimestamps = sunset;
+    const timezoneOffsets = timezone; 
 
-    const dates = new Date(sunriseUnixTimestamps * 1000); // Convert to milliseconds
-    const utcTimes = dates.getTime() + dates.getTimezoneOffset() * 60000; // Adjust for local time zone
-    const localTimes = new Date(utcTimes + timezoneOffsets * 1000); // Adjust for the provided time zone offset
+    const dates = new Date(sunriseUnixTimestamps * 1000); 
+    const utcTimes = dates.getTime() + dates.getTimezoneOffset() * 60000; 
+    const localTimes = new Date(utcTimes + timezoneOffsets * 1000); 
     const sunsetTime = localTimes.toTimeString().slice(0, 8);
 
 
@@ -64,55 +64,9 @@ document.querySelector('#find')
 document.querySelector('.search-bar').addEventListener("keyup", function (event) {
   if (event.key == "Enter") {
     weather.search();
+    document.querySelector(".search-bar").value = "";
   }
 });
 weather.fetchWeather();
 
 
-
-// data={
-//   "coord": {
-//   "lon": 77.2167,
-//   "lat": 28.6667
-//   },
-//   "weather": [
-//   {
-//   "id": 721,
-//   "main": "Haze",
-//   "description": "haze",
-//   "icon": "50n"
-//   }
-//   ],
-//   "base": "stations",
-//   "main": {
-//   "temp": 298.2,
-//   "feels_like": 298.14,
-//   "temp_min": 298.2,
-//   "temp_max": 298.2,
-//   "pressure": 1016,
-//   "humidity": 53
-//   },
-//   "visibility": 2500,
-//   "wind": {
-//   "speed": 0,
-//   "deg": 0
-//   },
-//   "clouds": {
-//   "all": 0
-//   },
-//   "dt": 1698851626,
-//   "sys": {
-//   "type": 1,
-//   "id": 9165,
-//   "country": "IN",
-//   "sunrise": 1698800560,
-//   "sunset": 1698840399
-//   },
-//   "timezone": 19800,
-//   "id": 1273294,
-//   "name": "Delhi",
-//   "cod": 200
-//   }
-
-//   console.log(data['name'])
-//   console.log(data['main']['temp'])
